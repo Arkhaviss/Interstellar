dbcee71330ad4d40b95cf2205b77bc178550c562
Kayden <160945338+Arkhaviss@users.noreply.github.com>
2024-12-05 07:05:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
+  Cylis: "Cylis164341",
   Kayden: "m10142010m",
+  Levi: "Evergreen85",
   River: "Something3655",
   Quentin: "Applesauce",
   },

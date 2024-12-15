457d54f3f71222e647a43aae89fed8513386516a
Arkhaviss <kasdeeeee@gmail.com>
2024-12-15 11:49:31 -0600

    Added Naejon back.

--- config.js
+++ config.js
@@ -2,13 +2,14 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-  Cylis: "Cylis164341",
-  Kayden: "m10142010m",
-  Levi: "Evergreen85",
-  Mason: "Hasard26!",
-  Quentin: "Applesauce",
-  River: "Something3655",
-  Sylis: "Therealguy",
+    Cylis: "Cylis164341",
+    Kayden: "m10142010m",
+    Levi: "Evergreen85",
+    Mason: "Hasard26!",
+    Naejon: "0913",
+    Quentin: "Applesauce",
+    River: "Something3655",
+    Sylis: "Therealguy",
   },
 };
 

ec0dade3a7d316b54c7fc5e80988ec90daf37dd4
Kayden <160945338+Arkhaviss@users.noreply.github.com>
2024-12-22 14:46:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Cylis: "Cylis164341",
+    Dean: "AddictedToLegos",
+    Kayden: "m10142010m",
+    Levi: "Evergreen85",
+    Mason: "Hasard26!",
+    Naejon: "0913",
+    Quentin: "Applesauce",
+    River: "Something3655",
+    Sylis: "Therealguy",
   },
 };
 
